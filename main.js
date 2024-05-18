24fd69459db25a699b853ec597ebf7c7e4309864
Likith Kandepu <withlikith@gmail.com>
2024-05-18 10:13:47 +0530

    new changes

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text",{
+    strings:["Full Stack Developer", "Student @ KL University", "Web Developer"],
+    typeSpeed : 10,
+    backSpeed : 100,
+    backDelay : 1000,
+    loop : true
+});
